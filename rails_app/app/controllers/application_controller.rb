cf92616b36ff6aca3edd2a3de8c9c227c6ab26bc
vk26 <vkosmos26@yandex.ru>
2017-02-14 11:23:50 +0000

    Setup reverse proxy to phoenix

--- rails_app/app/controllers/application_controller.rb
+++ rails_app/app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def proxy_to_phoenix
+    reverse_proxy "http://localhost:4000" do |config|
+      config.on_missing do |code, response|
+        redirect_to root_url and return
+      end
+    end
+  end
 end
